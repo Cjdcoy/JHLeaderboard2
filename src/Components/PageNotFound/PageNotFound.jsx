84c6c9af00693ad11e79dd8d29d83e73f7f72afe
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-10-28 12:07:09 +0300

    style: styling not found page

--- src/Components/PageNotFound/PageNotFound.jsx
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -14,8 +14,8 @@ const PageNotFound = () => {
     <main className={s.pageNotFound}>
       <img src={img404} alt="Page not found image" />
       <h1>This page doesn’t exist</h1>
-      <p>Please check your URL or return to home page.</p>
-      <button type="button">Go back to home page</button>
+      <p>Please check your URL or return to the home page.</p>
+      <button type="button">Return to Home page</button>
     </main>
   );
 };
