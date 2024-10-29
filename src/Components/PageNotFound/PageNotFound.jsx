2e4383032c9ded117e01dfed001ec438edf7c6c4
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-10-29 15:05:37 +0300

    style: improve not found page

--- src/Components/PageNotFound/PageNotFound.jsx
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -13,7 +13,7 @@ const PageNotFound = () => {
 
   return (
     <main className={s.pageNotFound}>
-      <img src={img404} alt="Page not found image" />
+      <img src={img404} alt="Error 404 not found" />
       <h1>This page doesn’t exist</h1>
       <p>Please check your URL or return to the home page.</p>
       <Link to="/">Return to Home page</Link>
